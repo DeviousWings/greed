1be39171a769cafdee617024b8762aad56d3d2e4
DeviousWings <nksteele@byui.edu>
2022-11-25 21:40:39 -0600

    first commit

---
+++ game/casting/artifact.py
@@ -0,0 +1,18 @@
+from game.casting.actor import Actor
+
+
+class Artifact(Actor):
+    def __int__(self):
+        super().__init__()
+    
+    def calculate_points(self):
+        """Calculates score"""
+        points = 0
+        
+        if (self.get_text() == '*'):
+            points = 1
+            
+        else: 
+            points = -1
+            
+        return points
